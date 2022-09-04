145e13c6991733ebbfbb3458e8623488c318e36a
rtaskayev <washingtonqwerty@gmail.com>
2022-09-04 13:28:49 +0200

    Calculator task

--- com/netology/Main.java
+++ com/netology/Main.java
@@ -20,6 +20,11 @@ class Main {
         calc.println.accept(a);
         calc.println.accept(b);
         calc.println.accept(c);
+
+        //calc.println.accept(d);
+        // Не понял почему код не должен был скомпилироваться. Единственная проблема возникла с предикатом, так как он выдает булево число.
+        // Пришлось добавить ниже операцию для приведения к интеджер при выводе
+
         calc.println.accept((d) ? 1 : 0);
         calc.println.accept(e);
         calc.println.accept(f);
